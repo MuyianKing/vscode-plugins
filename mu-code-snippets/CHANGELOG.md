306aff949e9b2c9a4364fb8ff0c5ac72f902e019
MuyianKing <muyianking@163.com>
2024-12-05 09:51:57 +0800

    增加html代码片段

--- mu-code-snippets/CHANGELOG.md
+++ mu-code-snippets/CHANGELOG.md
@@ -1,4 +1,7 @@
 ### TODO
 
+### 0.0.5
++ 增加html代码片段
+
 ### 0.0.4
 + 增加代码片段 mu_vue_table_column_slot ：hl-table-column默认插槽片段
--- mu-code-snippets/test/test.vue
+++ mu-code-snippets/test/test.vue
@@ -1,1 +1,11 @@
-]
+<script setup>
+</script>
+
+<template>
+  <div>
+   mu
+  </div>
+</template>
+
+<style lang='scss' scoped>
+</style>
